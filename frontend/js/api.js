48878f6ed71cca714176638e4a8d560667caa3ff
abhicsng007 <abhicsng007@gmail.com>
2026-01-31 14:24:24 +0530

    changed Api base url

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,7 +1,8 @@
 const API_BASE =
   window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000"
-    : window.API_BASE_URL;
+    : "https://hrms-lite-production.up.railway.app";
+
 
 
 async function apiRequest(url, options = {}) {
