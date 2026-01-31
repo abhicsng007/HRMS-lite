aff4ce4d97924294828988852b94b779ef204e1a
abhicsng007 <abhicsng007@gmail.com>
2026-01-31 14:33:57 +0530

    added origin in main.py

--- backend/main.py
+++ backend/main.py
@@ -16,9 +16,15 @@ app = FastAPI(title="HRMS Lite")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
 PORT = int(os.getenv("PORT", 8000))
 
+origins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://hrms-lite-eight.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins= origins ,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
