02b25658fabff8019f87b7906793a617216f3da7
abhicsng007 <abhicsng007@gmail.com>
2026-01-31 19:27:56 +0530

    added origin in main.py

--- backend/main.py
+++ backend/main.py
@@ -17,8 +17,8 @@ FRONTEND_URL = os.getenv("FRONTEND_URL")
 PORT = int(os.getenv("PORT", 8080))
 
 origins = [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
     "https://hrms-lite-eight.vercel.app",
 ]
 
